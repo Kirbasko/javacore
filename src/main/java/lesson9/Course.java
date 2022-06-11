e8e5be87c6a66a975f99496dadd2cfd953cd3557
kirbasko <kir.basko@gmail.com>
2022-06-11 22:15:14 +0300

    9 задание дз добавил

---
+++ src/main/java/lesson9/Course.java
@@ -0,0 +1,25 @@
+package lesson9;
+
+
+public class Course {
+    public String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
